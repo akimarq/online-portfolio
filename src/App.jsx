78b39482271a9a1cf6ab7b8654c7b2acfb08283d
//aki <akimarqueses@gmail.com>
2025-09-05 07:08:52 +0800

    Header and landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  <div className="body">
+    <div className="header">
+      <div className="header-text">
+        <h1>achilles marqueses</h1>
+        <p>Creative Developer</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+    </div>
+    
+    <div className="landing">
+      <div className="left">
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div className="right">
+        <div className="landing-image">
+          <img src="src/assets/portrait-up.jpg" alt="landing-image"/>
+        </div>
+      </div>
+    </div>
+
+
+  </div>
   )
 }
 
