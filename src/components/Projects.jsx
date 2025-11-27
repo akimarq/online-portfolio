8055907484b7840d2f3c762d35670cb629ba5197
//aki <akimarqueses@gmail.com>
2025-11-27 22:49:50 +0800

    projects section responsiveness
    not really responsiveness update, just added roles to the projects and adjusted the size on the desktop version

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,9 +9,9 @@ function Projects() {
           <h1>projects</h1>
         </div>
         <div className="projects-grid">
-          <Card name="PYTHOMANCER" desc="Top-down RPG based on Python Programming" env="GDevelop, JavaScript" />
-          <Card name="ANTI-KAIJU INSTITUTE" desc="3D Fighing Hack and Slash game based on Kaiju No.8" env="Unity, C#, Blender" />
-          <Card name="//.LAST STAND" desc="Cyberpunk-themed 2D Platformer Shooter" env="GDevelop" /> 
+          <Card name="PYTHOMANCER" desc="Top-down RPG based on Python Programming" env="GDevelop, JavaScript | Role: Lead Developer & Designer" />
+          <Card name="//.LAST STAND" desc="Cyberpunk-themed 2D Platformer Shooter" env="GDevelop | Role: Lead Developer & Designer" /> 
+          <Card name="ANTI-KAIJU INSTITUTE" desc="3D Fighing Hack and Slash game based on Kaiju No.8" env="Tech: Unity, C#, Blender | Role: Lead Developer & Designer" />
         </div>
       </div>
   );
