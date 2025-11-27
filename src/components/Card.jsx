3b26086b12130b8727c7bf8dc7aadfb0a5c83b61
//aki <akimarqueses@gmail.com>
2025-11-27 22:30:08 +0800

    projects section responsiveness v1
    added basic responsiveness to projects section, will improve further once the pages are complete

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,3 +1,4 @@
+
 function Card(project) {
     return (   
         <div className="project-card">
--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ createRoot(document.getElementById('root')).render(
     <App />
     <Header />
     <Landing />
-    {/* <Projects />
-    <About />  */}
+    <Projects />
+    {/* <About />  */}
   </StrictMode>,
 )
