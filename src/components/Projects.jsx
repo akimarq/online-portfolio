4769f9f0d5e393b593bcf8f2ae543b67ca54b39f
//aki <akimarqueses@gmail.com>
2025-12-13 04:08:55 +0800

    video 3
    progress 100%
    
    only minor adjustments left are for responsiveness

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,8 +3,10 @@ import './projects.css';
 import Card from './Card';
 import Video1 from '../assets/video1.mp4';
 import Video2 from '../assets/video2.mp4';
+import Video3 from '../assets/video3.mp4';
 import Thumb1 from '../assets/thumb1.png';
 import Thumb2 from '../assets/thumb2.png';
+import Thumb3 from '../assets/thumb3.png';
 
 function Projects({sectionRef}) {
   return (
@@ -15,7 +17,7 @@ function Projects({sectionRef}) {
         </div>
         <div className="projects-grid">
           <Card name="PYTHOMANCER" desc="Top-down RPG based on Python Programming" env="GDevelop, JavaScript | Role: Lead Developer & Designer" videoSrc={Video1} poster={Thumb1} />
-          <Card name="//.LAST STAND" desc="Cyberpunk-themed 2D Platformer Shooter" env="GDevelop | Role: Lead Developer & Designer" />
+          <Card name="//.LAST STAND" desc="Cyberpunk-themed 2D Platformer Shooter" env="GDevelop | Role: Lead Developer & Designer" videoSrc={Video3} poster={Thumb3} />
           <Card name="ANTI-KAIJU INSTITUTE" desc="3D Fighing Hack and Slash game based on Kaiju No.8" env="Tech: Unity, C#, Blender | Role: Lead Developer & Designer" videoSrc={Video2} poster={Thumb2} />
         </div>
       </div>
